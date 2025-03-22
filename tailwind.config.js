32a5c6c89f8b388a8856af1e0c0339d376b8f794
robertootupacca <roberto.otupacca@edu.ti.ch>
2025-03-23 00:58:40 +0100

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class', 
+    
+    presets: [
+        require('./vendor/tallstackui/tallstackui/tailwind.config.js') 
+    ],
+
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './app/View/Components/**/*.php', 
+        './vendor/tallstackui/tallstackui/src/**/*.php', 
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, typography],
+};
+
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import {defineConfig, loadEnv} from 'vite'
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({command, mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
@@ -10,8 +9,7 @@ export default defineConfig(({command, mode}) => {
             laravel({
                 input: ['resources/css/app.css', 'resources/js/app.js'],
                 refresh: [`resources/views/**/*`],
-            }),
-            tailwindcss(),
+            })
         ],
         server: {
             cors: true,
