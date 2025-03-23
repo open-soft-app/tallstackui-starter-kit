bda08778cfbb88d00a4bd7675a5622c343caf88b
robertootupacca <roberto.otupacca@edu.ti.ch>
2025-03-23 01:12:59 +0100

    First Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,26 +7,45 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <tallstackui:script /> 
-        @livewireStyles
         <!-- Scripts -->
+        <tallstackui:script /> 
         @vite(['resources/css/app.css', 'resources/js/app.js'])        
+        <!-- Styles -->
+        @livewireStyles
     </head>
-    <body>
-        <div>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @isset($header)
+            {{-- @isset($header)
                 <header>
                     <div>
                         {{ $header }}
                     </div>
                 </header>
-            @endisset
+            @endisset --}}
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white dark:bg-gray-800 shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
 
             <!-- Page Content -->
+            {{-- <main>
+                {{ $slot }}
+            </main> --}}
+            
+            <!-- Page Content -->
             <main>
+                
+                <x-ts-checkbox label="Receive Alert" />
+
+                <x-ts-checkbox label="Receive Alert" position="left" />
+                
                 {{ $slot }}
             </main>
         </div>
